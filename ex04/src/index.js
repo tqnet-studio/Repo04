024efca7f40991af91f082d20c31e80d9eb360cc
Marko <marko.saraba@gmail.com>
2022-03-31 20:34:58 +0200

    dodato par vjezbi

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+
+//only change code below this line
+var myDecimal;
+var myDecimalE;
+ 
+myDecimal = 8.2;
+myDecimalE = 254e-5;
+
+ //only change code above this line
+return {
+    myDecimal,
+    myDecimalE
+       };
+}
+
+console.log(main());
+module.exports = main;
